488883e37207f4a05144908ae36b24b2523830db
moak13 <michaelkalango@gmail.com>
2022-08-21 15:51:35 +0100

    routing and theme engine setup

---
+++ theme_switch/lib/core/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import '../../features/home/home_view.dart';
+import '../../features/splash/splash_view.dart';
+import '../services/theme_switch_service.dart';
+
+@StackedApp(routes: [
+  AdaptiveRoute(
+    page: SplashView,
+    initial: true,
+  ),
+  AdaptiveRoute(
+    page: HomeView,
+  ),
+], dependencies: [
+  LazySingleton(
+    classType: ThemeSwitchService,
+  ),
+  LazySingleton(
+    classType: NavigationService,
+  ),
+])
+class AppSetup {}
---
+++ theme_switch/lib/core/services/theme_switch_service.dart
@@ -0,0 +1,7 @@
+import 'package:stacked_themes/stacked_themes.dart';
+
+class ThemeSwitchService extends ThemeService {
+  ThemeSwitchService() {
+    ThemeService.getInstance();
+  }
+}
